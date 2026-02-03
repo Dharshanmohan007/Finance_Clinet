f41c17695563bf6530b11795aefe8c63217c8fab
dharshan-m <dharshanmohan2704@gmail.com.com>
2026-02-03 16:40:56 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import Landing from './pages/Landing'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
